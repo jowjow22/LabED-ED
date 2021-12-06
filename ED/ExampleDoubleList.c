aba08ac31c6c14c2155d0238d7ccc648f4d8ccb6
jowjow22 <jonathan224santos@gmail.com>
2021-12-05 23:12:51 -0300

    feat: add middle insert function

--- ED/ExampleDoubleList.c
+++ ED/ExampleDoubleList.c
@@ -9,7 +9,8 @@ typedef struct DoubleWayList
   struct DoubleWayList *next;
 } DWList;
 /*
-esse struct tem a diferença de ter um outro ponteiro, ele tem um que aponta para o proximo e outro que aponta para o anterior
+A primeira diferença notável é que temos dois ponteiros de struct, um apontando para o proximo, e outro para o anterior, por isso
+duplamente encadeada
 */
 
 DWList *allocates(int key)
@@ -28,8 +29,7 @@ DWList *allocates(int key)
   }
 }
 /*
-  a unica alteracao necessaria foi a adicao de uma nova atribuicao, no caso atribui-se nulo ao valor do ponteiro que referencia o anterior
-  a ele.
+Aqui a unica diferença é que foi adicionada uma nova atribuição, da qual é o ponteiro de anterior
 */
 
 DWList *initInsert(DWList *first, int key)
@@ -43,9 +43,10 @@ DWList *initInsert(DWList *first, int key)
   return newNode;
 }
 /*
-  Como estamos inserindo no inicio, e a posicao previa ja foi definida como nula, nao precisamos definir como nula novamente
-  apenas atribuimos a proxima posiçao como a antiga primeira posicao da lista. Aqui temos uma alteracao na funcao, caso a lista
-  nao esteja vazia, definimos o ponteiro anterior da primeira posicao da lista, como o novo Node que estamos criando
+  Como estamos inserindo no início, e a posição prévia já foi definida como nula, não precisamos definir como nula novamente
+  apenas atribuímos a próxima posição como a antiga primeira posicao da lista,e atribuímos isso ao ponteiro da Main. Aqui temos
+  uma alteração na função, caso a lista não esteja vazia, definimos o ponteiro anterior da primeira posição da lista, como o novo 
+  Node que estamos criando
 */
 
 void printNodeList(DWList *first)
@@ -63,6 +64,10 @@ void printNodeList(DWList *first)
     }
   }
 }
+/*
+Aqui temos uma alteração da qual não é necessária, em vez de usar o laço while, utilizamos o laço for, fazendo com que nosso aux percorra desde
+a primeira posição, até que a lista seja nula, passando de nó em nó. Porém isso poderia ser feito com while também.
+*/
 void printNodeListBackway(DWList *last)
 {
   DWList *auxNode;
@@ -75,6 +80,11 @@ void printNodeListBackway(DWList *last)
     printf("Key: %d\n", auxNode->key);
   }
 }
+/*
+  Esta função serve para imprimir a lista de trás para frente. Ela recebe como parametro, o ultimo elemento da lista. Utilizando o laço for
+  percorremos a lista desde o ultimo elemento atribuído, até que ele seja nulo, utilizando o campo previous ou anterior, da nossa lista duplamente
+  encadeada.
+*/
 
 DWList *searchInList(DWList *first, int key)
 {
@@ -88,6 +98,11 @@ DWList *searchInList(DWList *first, int key)
   }
   return NULL;
 }
+
+/*
+  Esta função tem como diferenças principais, a utilização do laço for em vez de while para percorrer a lista, e támbem usa apenas um ponteiro
+  em vez de um ponteiro de ponteiro. Ela realiza a busca por chave ou dado do struct.
+*/
 
 DWList *searchInListPosition(DWList *first, int pos)
 {
@@ -104,6 +119,27 @@ DWList *searchInListPosition(DWList *first, int pos)
   }
   return NULL;
 }
+
+void locatedInsertion(DWList *first, int pos, int key)
+{
+  DWList *aux = searchInListPosition(first, pos - 1);
+  if (aux != NULL)
+  {
+    DWList *newNode = allocates(key);
+    newNode->next = aux->next;
+    aux->next = newNode;
+  }
+  else
+  {
+    printf("Invalid Position!\n");
+  }
+}
+/*
+ Esta função foi criada para auxiliar na função de remover no meio, ela é uma busca que retorna o nó por posição, no arquivo de lista simples
+ ela foi implementada juntamente da função que remove no meio, aqui somente foi separada para melhor organização, mas basicamente
+ ela percorre a lista e verifica se a posição atual que é contada pelo contador i, é igual a posição desejada, caso seja, ela retorna o nó atual,
+ caso não seja, ela retorna nulo
+*/
 DWList *removeFirst(DWList **first)
 {
   DWList *aux = *first;
@@ -117,6 +153,10 @@ DWList *removeFirst(DWList **first)
   }
   return NULL;
 }
+/*
+  A principal diferença dessa função, é que criamos outro ponteiro auxiliar, para receber o próximo nó do primeiro, e com isso após removermos o primeiro
+  nó, colocamos o anterior desse outro ponteiro como nulo.
+*/
 void removeFromList(DWList **first, int pos)
 {
   DWList *toErase, *prev, *next;
@@ -145,10 +185,17 @@ void removeFromList(DWList **first, int pos)
     }
   }
 }
+/*
+  Esta função tem como diferenças principais, a utilização do laço for em vez de while para percorrer a lista, e támbem usa apenas um ponteiro
+  em vez de um ponteiro de ponteiro.
+*/
 
 int main()
 {
   DWList *first = NULL, *last = NULL;
+  /*
+  Temos uma 
+  */
   first = initInsert(first, 9);
   last = first;
   first = initInsert(first, 3);
@@ -163,6 +210,9 @@ int main()
   printNodeList(first);
   first = removeFirst(&first);
   printf("\n*************************\n");
+  printNodeList(first);
+  printf("\n*************************\n");
+  locatedInsertion(first, 2, 13);
   printNodeList(first);
   printf("\n*************************\n");
 }
